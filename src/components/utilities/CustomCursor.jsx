e4fe4e87dbeb280ff97cdd66fd0232215713c14d
RochakDhakal <rochakdhakal@gmail.com>
2023-09-24 17:25:49 +0545

    cursor sm-md:hidden

--- src/components/utilities/CustomCursor.jsx
+++ src/components/utilities/CustomCursor.jsx
@@ -21,11 +21,11 @@ function CustomCursor() {
   return (
     <>
       <div
-        className="fixed cursor-none  pointer-events-none border-gray-500 border h-3 w-3 p-4 z-50 rounded-full duration-200 transition-all ease-linear"
+        className="hidden lg:block fixed cursor-none  pointer-events-none border-gray-500 border h-3 w-3 p-4 z-50 rounded-full duration-200 transition-all ease-linear"
         style={{ top: mouseAxis.y - 9, left: mouseAxis.x - 9 }}
       ></div>
       <div
-        className="fixed cursor-none h-2 w-2 p-2 backdrop-invert rounded-full z-50 pointer-events-none"
+        className="hidden lg:block fixed cursor-none h-2 w-2 p-2 backdrop-invert rounded-full z-50 pointer-events-none"
         style={{ top: mouseAxis.y, left: mouseAxis.x }}
       ></div>
     </>
